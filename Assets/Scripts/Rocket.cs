6843b4a5d344a0e680d61f4487252a00760f3328
Darker <wkc419@hotmail.com>
2020-04-05 14:44:39 +0100

    End of Chapter 59

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -6,60 +6,108 @@ using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
-    AudioSource rocketSound;
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float thrustScale = 1f;
-    [SerializeField] int fuelLevel = 999;
+    [SerializeField] int fuelUseRate = 1;
+    [SerializeField] float fuelLevel = 999f;
     [SerializeField] int lives = 3;
+    [SerializeField] int level;
+    [SerializeField] float dyingTime = 1f;
+    [SerializeField] float transcendingTime = 1f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    Scene scene;
+
+
+    enum State { Alive, Dying, Transcending };
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        rocketSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rotate();
-        Thrust();
-        DetectCollision();
+        if (state == State.Alive) {
+            RespondToRotateInput();
+            RespondToThrustInput();
+        }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) {return;} //Ignore collisions if not alive.
+
         switch (collision.gameObject.tag)
         {
             case "Respawn":
-                // DO NOTHING
                 break;
             case "Finish":
-                // DO NOTHING
-                SceneManager.LoadScene(1);
-                break;
-            case "Fuel":
-                // Collect fuel
-                fuelLevel += 100;
-                print("Fuel Collected");
+                StartSuccessSequence();
                 break;
             default:
-                print("Dead");
-                SceneManager.LoadScene(0);
-                //TODO Kill player
+                StartDeathSequence();
                 break;
         }
     }
 
+    void OnTriggerEnter(Collider Collider)
+    {
+        if (Collider.gameObject.tag == "Fuel")
+        {
+            fuelLevel += 100;
+            Collider.gameObject.SetActive(false);
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", transcendingTime);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        rigidBody.constraints = RigidbodyConstraints.None;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", dyingTime);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+        state = State.Alive;
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+        state = State.Alive;
+    }
 
 
-    private void Rotate()
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; //Take manual control of rotation
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) {
+
+        }
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
@@ -67,28 +115,31 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(Vector3.back * rotationThisFrame);
         }
-
         rigidBody.freezeRotation = false; //Resume physics control of rotation
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void ApplyThrust()
     {
         if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up * thrustScale);
+            fuelLevel = fuelLevel - (Time.deltaTime * fuelUseRate);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rocketSound.Play(); //Turn sounds on
-        }
-        else if (Input.GetKeyUp(KeyCode.Space))
-        {
-            rocketSound.Stop(); //Turn Sound Off
-        }
-    }
-
-    private void DetectCollision()
-    {
-        
     }
 }
